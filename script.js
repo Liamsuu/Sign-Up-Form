5a2d9c7aa3776ffeb99d284c74c9747081d30260
Liam <liamsuu14974@gmail.com>
2024-03-29 21:44:39 +0000

    Add basic password field validation

---
+++ script.js
@@ -0,0 +1,16 @@
+const pass = document.querySelector("#password");
+const confPass = document.querySelector("#conf-password");
+// const confButton = document.querySelector("button");
+const form = document.querySelector("form");
+const validationText = document.createElement("p");
+validationText.textContent = "* Passwords do not match";
+validationText.className = "pass-error";
+
+form.addEventListener("submit", (event) => {
+  if (pass.value !== confPass.value) {
+    pass.setAttribute("isValid", false);
+    confPass.setAttribute("isValid", false);
+    event.preventDefault();
+    pass.parentNode.insertBefore(validationText, pass.nextSibling);
+  }
+});
